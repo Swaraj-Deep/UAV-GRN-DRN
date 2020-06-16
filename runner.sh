17abb84a5d3b1c3ade51320a3b4409191dbb1769
Krishnandu91 <66660004+Krishnandu91@users.noreply.github.com>
2020-06-16 16:18:07 +0530

    Add files via upload

---
+++ runner.sh
@@ -0,0 +1,8 @@
+for i in $(seq 1 20); do
+  echo Starting to execute main.py
+  warning=$(python3 main.py 2>&1)
+  echo Executed main.py $i times.
+done
+
+echo Analysing the Results
+python3 analysis.py
