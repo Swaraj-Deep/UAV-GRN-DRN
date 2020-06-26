b9a260075c20cf6ce5d3d0a512929720684def90
Swaraj-Deep <swarajdeep33445@gmail.com>
2020-06-26 19:49:08 +0530

    Prettified output files

--- analysis.py
+++ analysis.py
@@ -17,8 +17,9 @@ def analyse_output_files():
         learning_rate = file_data['learning_rate']
         decay_factor = file_data['decay_factor']
     parent_dir = './output_files'
-    curr_dir = str(epsilon) + "_" + str(learning_rate) + "_" + str(decay_factor)
-    dir_path = os.path.join (parent_dir, curr_dir)
+    curr_dir = str(epsilon) + "_" + str(learning_rate) + \
+        "_" + str(decay_factor)
+    dir_path = os.path.join(parent_dir, curr_dir)
     # os.chdir(dir_path)
     text_files = []
     minm_user_served = 999999999
@@ -45,20 +46,30 @@ def analyse_output_files():
                 maxm_user_served = curr_user_served
                 best_file = dir_path + "/" + file
     list_line_to_write = []
-    list_line_to_write.append(f'############################################################################################\n')
-    list_line_to_write.append(f'################################### Analysis Report ########################################\n')
-    list_line_to_write.append(f'############################################################################################\n')
+    list_line_to_write.append(
+        f"Input Scenario\nLearning Rate: {learning_rate}\nExploration Rate: {epsilon}\nDecay Factor: {decay_factor}\n")
+    list_line_to_write.append(
+        f'############################################################################################\n')
+    list_line_to_write.append(
+        f'################################### Analysis Report ########################################\n')
+    list_line_to_write.append(
+        f'############################################################################################\n')
     best_graph = dir_path + '/Output_graph' + \
         best_file[26:len(best_file) - 4:] + '.png'
-    list_line_to_write.append(f'# Location of Best Output file is: {best_file}\n')
-    list_line_to_write.append(f'# Corresponding Graph file location is: {best_graph}\n')
+    list_line_to_write.append(
+        f'Location of Best Output file is: {best_file}\n')
+    list_line_to_write.append(
+        f'Corresponding Graph file location is: {best_graph}\n')
     worst_graph = dir_path + '/Output_graph' + \
         worst_file[26:len(worst_file) - 4:] + '.png'
     list_line_to_write.append(
-        f'# Location of Worst Output file is: {worst_file}\n')
-    list_line_to_write.append(f'# Corresponding Graph file location is: {worst_graph}\n')
-    list_line_to_write.append(f'# Mean User Served: {sum_user_served / total_files}\n')
-    list_line_to_write.append("############################################################################################\n")
+        f'Location of Worst Output file is: {worst_file}\n')
+    list_line_to_write.append(
+        f'Corresponding Graph file location is: {worst_graph}\n')
+    list_line_to_write.append(
+        f'Mean User Served: {sum_user_served / total_files}\n')
+    list_line_to_write.append(
+        "############################################################################################\n")
     with open(dir_path + "/" + "analysis.log", 'w') as file_pointer:
         file_pointer.writelines(list_line_to_write)
 
