6c148972c6f5a268b8b66627812930c86c6c86c9
Swaraj-Deep <swarajdeep33445@gmail.com>
2020-06-26 19:37:03 +0530

    Done Analysis Part

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import os
+import json
 import os.path
 
 
@@ -7,13 +8,18 @@ def analyse_output_files():
     Function: analyse_output_files\n
     Functionality: Calculate the mean of the output files, provides the best and the worst output files\n
     """
+    epsilon = 0.0
+    learning_rate = 0.0
+    decay_factor = 0.0
     with open('input_files/scenario_input.json', 'r') as file_pointer:
         file_data = json.load(file_pointer)
         epsilon = file_data['epsilon']
         learning_rate = file_data['learning_rate']
         decay_factor = file_data['decay_factor']
-    parent_dir = os.getcwd ()
-    
+    parent_dir = './output_files'
+    curr_dir = str(epsilon) + "_" + str(learning_rate) + "_" + str(decay_factor)
+    dir_path = os.path.join (parent_dir, curr_dir)
+    # os.chdir(dir_path)
     text_files = []
     minm_user_served = 999999999
     maxm_user_served = -99999999
@@ -22,56 +28,39 @@ def analyse_output_files():
     best_file = ''
     sum_user_served = 0
     total_files = 0
-    for file in os.listdir('./output_files'):
+    for file in os.listdir(dir_path):
         name, ext = os.path.splitext(file)
         if ext == '.txt':
             text_files.append(name + ext)
             total_files += 1
     for file in text_files:
-        with open('./output_files/' + file, 'r') as file_pointer:
+        with open(dir_path + "/" + file, 'r') as file_pointer:
             lines = file_pointer.readlines()
             curr_user_served = int(lines[-2].split(':')[1])
             sum_user_served += curr_user_served
             if curr_user_served < minm_user_served:
                 minm_user_served = curr_user_served
-                worst_file = './output_files/' + file
+                worst_file = dir_path + "/" + file
             if curr_user_served > maxm_user_served:
                 maxm_user_served = curr_user_served
-                best_file = './output_files/' + file
-    print(f'############################################################################################')
-    print(f'################################### Analysis Report ########################################')
-    print(f'############################################################################################')
-    op = len(f'# Location of Best Output file is: {best_file}')
-    best_graph = './output_files/Output_graph' + \
+                best_file = dir_path + "/" + file
+    list_line_to_write = []
+    list_line_to_write.append(f'############################################################################################\n')
+    list_line_to_write.append(f'################################### Analysis Report ########################################\n')
+    list_line_to_write.append(f'############################################################################################\n')
+    best_graph = dir_path + '/Output_graph' + \
         best_file[26:len(best_file) - 4:] + '.png'
-    print(f'# Location of Best Output file is: {best_file}', end='')
-    for i in range((91 - op) // 2):
-        print(' ', end=' ')
-    print(' #')
-    print(f'# Corresponding Graph file location is: {best_graph}', end='')
-    op = len(f'# Corresponding Graph file location is: {best_graph}')
-    for i in range((91 - op) // 2):
-        print(' ', end=' ')
-    print(' #')
-    worst_graph = './output_files/Output_graph' + \
+    list_line_to_write.append(f'# Location of Best Output file is: {best_file}\n')
+    list_line_to_write.append(f'# Corresponding Graph file location is: {best_graph}\n')
+    worst_graph = dir_path + '/Output_graph' + \
         worst_file[26:len(worst_file) - 4:] + '.png'
-    print(
-        f'# Location of Worst Output file is: {worst_file}', end='')
-    op = len(f'# Location of Worst Output file is: {worst_file}')
-    for i in range((91 - op) // 2):
-        print(' ', end=' ')
-    print(' #')
-    print(f'# Corresponding Graph file location is: {worst_graph}', end='')
-    op = len(f'# Corresponding Graph file location is: {worst_graph}')
-    for i in range((91 - op) // 2):
-        print(' ', end=' ')
-    print('#')
-    print(f'# Mean User Served: {sum_user_served / total_files}', end='')
-    op = len(f'# Mean User Served: {sum_user_served / total_files}')
-    for i in range((91 - op) // 2):
-        print(' ', end=' ')
-    print('#')
-    print("############################################################################################")
+    list_line_to_write.append(
+        f'# Location of Worst Output file is: {worst_file}\n')
+    list_line_to_write.append(f'# Corresponding Graph file location is: {worst_graph}\n')
+    list_line_to_write.append(f'# Mean User Served: {sum_user_served / total_files}\n')
+    list_line_to_write.append("############################################################################################\n")
+    with open(dir_path + "/" + "analysis.log", 'w') as file_pointer:
+        file_pointer.writelines(list_line_to_write)
 
 
 if __name__ == "__main__":
