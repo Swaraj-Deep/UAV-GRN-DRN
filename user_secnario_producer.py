81e011376204a200569b30a5eaaf9d627bf2a1f0
Swaraj-Deep <swarajdeep33445@gmail.com>
2020-07-23 12:26:00 +0530

    Added code for random user generation

--- user_secnario_producer.py
+++ user_secnario_producer.py
@@ -9,7 +9,7 @@ def generate_user_input(N, M, number_users):
     Function: generate_user_input\n
     Parameter: (N, M) -> size of the grid, number_users -> Number of ground user needed to be placed\n
     Returns: nothing\n
-    Functionality: Writes the output to file_name_passed and saves it in input_files\n
+    Functionality: Writes the output to user_input file and update scenario file\n
     """
     user_pos = []
     while len(user_pos) < number_users:
@@ -22,14 +22,27 @@ def generate_user_input(N, M, number_users):
         "Position of Ground users": user_pos
     }
     parent_dir = os.getcwd()
-    file_name = str(N) + "_" + str(M) + "_" + str(number_users) + "_user.json"
-    file_path = os.path.join(parent_dir, 'input_files', 'user_input_scenarios', file_name)
+    file_name = "user_input.json"
+    file_path = os.path.join(parent_dir, 'input_files', file_name)
     with open(file_path, 'w') as file_pointer:
         json.dump(write_data, file_pointer)
+    file_name = "scenario_input.json"
+    file_path = os.path.join(parent_dir, 'input_files', file_name)
+    with open(file_path, 'r') as file_pointer:
+        file_data = json.load(file_pointer)
+    file_data["N"] = N
+    file_data["M"] = M
+    print (file_data)
+    with open(file_path, 'w') as file_pointer:
+        json.dump(file_data, file_pointer)
 
 
 if __name__ == "__main__":
-    N = int(input("Enter the number of Rows in the grid: "))
-    M = int(input("Enter the number of Columns in the grid: "))
-    number_users = int(input("Enter the number of ground users: "))
-    generate_user_input(N, M, number_users)
+    parent_dir = os.getcwd()
+    file_name = 'user_location.json'
+    file_path = os.path.join (parent_dir, 'input_files', file_name)
+    with open(file_path, 'r') as file_pointer:
+        file_data = json.load(file_pointer)
+        N = file_data['N']
+        number_users = file_data['Number of User']
+    generate_user_input(N, N, number_users)
