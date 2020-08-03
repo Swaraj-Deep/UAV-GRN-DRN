c2661bcb5e89f4742bb406df3683b78bf29a04f2
Swaraj-Deep <swarajdeep33445@gmail.com>
2020-08-03 11:30:25 +0530

    Made runner quicker

---
+++ __update.py
@@ -0,0 +1,35 @@
+import os
+import os.path
+import sys
+
+def update_main_file (type):
+    """
+    Function: update_main_file\n
+    Parameters: type -> whether bruteforce or q_learn\n
+    Functionality: Update Main file according to bruteforce or q_learn
+    """
+    parent_dir = os.getcwd()
+    file_name = 'main.py'
+    file_path = os.path.join(parent_dir, file_name)
+    file_data = []
+    with open(file_path, 'r') as file_pointer:
+        file_data = file_pointer.readlines()
+        line_to_change = file_data[263].split('=')
+        if type == 'bruteforce':
+            line_to_change[1] = 'bruteforce(UAV_node, placed)\n'
+        else:
+            line_to_change[1] = 'q_learn(UAV_node, placed)\n'
+    file_data[263] = '= '.join (line_to_change)
+    with (open(file_path, 'w')) as file_pointer:
+        file_pointer.writelines(file_data)
+    
+
+def call_update():
+    """
+    Function: call_update\n
+    Parameters: None\n
+    Functionality: To call the update function
+    """
+    update_main_file(sys.argv[1])
+
+call_update()
--- runner.sh
+++ runner.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
+read -p "Which process you want to run (bruteforce/q_learn): " type_input
+read -p "Enter number of times to run simulation: " number
+if [[ ! $number =~ ^[0-9]+$ ]]; then
+  echo "Enter only integers"
+  exit
+fi
 read -p "Do you want to delete previous output files (y/n): " input
 if [ ${input,,} = y ]; then
   echo "Removing previous output files"
   bash fresh_analysis.sh
   echo "Removed"
 fi
-for i in $(seq 1 5); do
+if [ ${type_input,,} == bruteforce ]; then
+  echo "Updating to bruteforce"
+  python3 __update.py bruteforce
+  for i in $(seq 1 $number); do
+    echo Generating user location
+    python3 user_secnario_producer.py
+    echo Generated user location
+    echo Starting to execute main.py
+    warning=$(python3 main.py 2>&1)
+    echo Executed main.py $i times.
+  done
+else
+  echo "Updating to q_learn"
+  python3 __update.py q_learn
   echo Generating user location
   python3 user_secnario_producer.py
   echo Generated user location
-  echo Starting to execute main.py
-  warning=$(python3 main.py 2>&1)
-  echo Executed main.py $i times.
-done
-
+  for i in $(seq 1 $number); do
+    echo Starting to execute main.py
+    warning=$(python3 main.py 2>&1)
+    echo Executed main.py $i times.
+  done
+fi
 echo Analysing the Results
 python3 analysis.py
