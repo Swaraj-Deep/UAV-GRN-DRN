dbe210d5601674531378b5a6948d84f2c34cd770
Swaraj-Deep <swarajdeep33445@gmail.com>
2020-11-11 20:45:26 +0530

    Change GU_Similarity_plot.py

--- GU_Similarity_plot.py
+++ GU_Similarity_plot.py
@@ -101,7 +101,8 @@ def line_plot(graph_data):
     plt.xticks(fontsize=10, fontweight='bold')
     plt.yticks(fontsize=10, fontweight='bold')
     plt.legend()
-    plt.savefig(file_path)
+    # plt.savefig(file_path)
+    plt.show()
     # Clear plot
     plt.clf()
     # Similarity Percentage
@@ -117,8 +118,59 @@ def line_plot(graph_data):
     plt.legend()
     file_name = 'SimiPerctNewPlot.png'
     file_path = os.path.join(parent_dir, dir_name, file_name)
-    plt.savefig(file_path)
+    # plt.savefig(file_path)
+    plt.show()
     
+    
+def all_plot(data1, data2, data3):
+    parent_dir = os.getcwd()
+    dir_name = 'analysis_output_files'
+    file_name = 'GUNewPlot.png'
+    file_path = os.path.join(parent_dir, dir_name, file_name)
+    x_val = [key for key in data1]
+    ground_user_covered = [value[0] for key, value in data1.items()]
+    plt.scatter(x_val, ground_user_covered)
+    plt.plot(x_val, ground_user_covered, label="Ground User served 100")
+    ground_user_covered = [value[0] for key, value in data2.items()]
+    plt.scatter(x_val, ground_user_covered)
+    plt.plot(x_val, ground_user_covered, label="Ground User served 200")
+    ground_user_covered = [value[0] for key, value in data3.items()]
+    plt.scatter(x_val, ground_user_covered)
+    plt.plot(x_val, ground_user_covered, label="Ground User served 300")
+    plt.ylabel('Percentage of Ground User covered',
+               fontsize=12, fontweight='bold')
+    plt.xlabel('Number of UAVs', fontsize=12, fontweight='bold')
+    plt.title('User coverage percentage Vs Number of UAVs',
+              fontsize=12, fontweight='bold')
+    plt.xticks(fontsize=10, fontweight='bold')
+    plt.yticks(fontsize=10, fontweight='bold')
+    plt.legend()
+    # plt.savefig(file_path)
+    plt.show()
+    # Clear plot
+    # plt.clf()
+    # # Similarity Percentage
+    similarity_percentage = [value[1] for key, value in data1.items()]
+    plt.scatter(x_val, similarity_percentage)
+    plt.plot(x_val, similarity_percentage, label="Similarity Percentage 100")
+    similarity_percentage = [value[1] for key, value in data2.items()]
+    plt.scatter(x_val, similarity_percentage)
+    plt.plot(x_val, similarity_percentage, label="Similarity Percentage 300")
+    similarity_percentage = [value[1] for key, value in data3.items()]
+    plt.scatter(x_val, similarity_percentage)
+    plt.plot(x_val, similarity_percentage, label="Similarity Percentage 300")
+    plt.ylabel('Similarity Percentage',
+               fontsize=12, fontweight='bold')
+    plt.xlabel('Number of UAVs', fontsize=12, fontweight='bold')
+    plt.title('Similarity percentage Vs Number of UAVs',
+              fontsize=12, fontweight='bold')
+    plt.xticks(fontsize=10, fontweight='bold')
+    plt.yticks(fontsize=10, fontweight='bold')
+    plt.legend()
+    file_name = 'SimiPerctNewPlot.png'
+    file_path = os.path.join(parent_dir, dir_name, file_name)
+    # plt.savefig(file_path)
+    plt.show()
 
 
 def write_to_json(data, file_path):
@@ -171,4 +223,12 @@ if __name__ == "__main__":
         os.mkdir(dir_path)
     except OSError as error:
         pass
-    init()
+    # init()
+    data_100 = {"10": [75.0, 12.5], "20": [95.0, 63.04347826086957], "30": [97.0, 77.21518987341773], "40": [99.0, 83.1858407079646], "50": [100.0, 85.62573099415205], "60": [100.0, 88.9344262295082], "70": [100.0, 91.3907284768212], "80": [100.0, 92.71708683473389], "90": [100.0, 93.54838709677419], "100": [100.0, 92.92035398230088]}
+    data_200 = {"10": [70.5, 10.5], "20": [94.0, 45.65217391304348], "30": [97.0, 60.75949367088608], "40": [98.5, 66.3716814159292], "50": [99.9, 71.9298245614035], "60": [100.0, 74.90163934426229], "70": [100.0, 80.4635761589404], "80": [100.0, 84.03361344537815], "90": [100.0, 87.09677419354838], "100": [100.0, 90.04424778761062]}
+    data_300 = {"10": [64.33333333333333, 6.25], "20": [84.0, 39.130434782608695], "30": [94.0, 57.29113924050633], "40": [96.66666666666667, 63.716814159292035], "50": [98.33333333333333, 66.51461988304094], "60": [100.0, 73.08196721311475], "70": [100.0, 79.13907284768213], "80": [100.0, 82.91316526610645], "90": [100.0, 85.60794044665012], "100": [100.0, 87.38938053097345]}
+    # line_plot(data_100)
+    # line_plot(data_200)
+    # line_plot(data_300)
+    all_plot(data_100, data_200, data_300)
+
